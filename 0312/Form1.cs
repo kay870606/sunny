c8daa2ca47081b11f0681e90e2abc6ddd85edd39
Kay <kay870606@gmail.com>
2020-03-26 12:35:06 +0800

    Second

--- 0312/Form1.cs
+++ 0312/Form1.cs
@@ -12,9 +12,7 @@ namespace _0312
 {
     public partial class Form1 : Form
     {
-        double operand = 0, lastOperand = 0;
         Boolean optrPressed = false, equalPressed = false, isDot = false;
-        int lastOperator = 0;
 
         public Form1()
         {
@@ -111,10 +109,6 @@ namespace _0312
             }
             return outstack.Peek();
         }
-        private void calc()
-        {
-
-        }
 
         private void buttonOperate_Click(object sender, EventArgs e)
         {
@@ -141,42 +135,18 @@ namespace _0312
                 }
 
                 textBoxResult.Text = eval(inqueue).ToString();
-
                 textBoxExpression.Text += button.Text + " ";
-                /*if (lastOperator == 0)
-                {
-                    
-                }
-                else if (lastOperator == 1)
-                {
-                    textBoxResult.Text = (operand - Convert.ToDouble(textBoxResult.Text)).ToString();
-                }
-                else if (lastOperator == 2)
-                {
-                    textBoxResult.Text = (operand * Convert.ToDouble(textBoxResult.Text)).ToString();
-                }*/
             }
-
-            if (button.Text == "+")
-                lastOperator = 0;
-            else if (button.Text == "-")
-                lastOperator = 1;
-            else if (button.Text == "*")
-                lastOperator = 2;
-            else if (button.Text == "/")
-                lastOperator = 3;
 
             equalPressed = false;
             optrPressed = true;
+            isDot = false;
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
             textBoxResult.Text = "0";
             textBoxExpression.Text = "";
-            operand = 0;
-            lastOperand = 0;
-            lastOperator = 0;
             optrPressed = false;
             equalPressed = false;
             isDot = false;
@@ -200,23 +170,7 @@ namespace _0312
                     }
 
                     textBoxResult.Text = eval(inqueue).ToString();
-                    textBoxExpression.Text = textBoxResult.Text + " " + expression[1] + " " + expression[2] + " " + "=" + " ";
-                    /*operand = Convert.ToDouble(textBoxResult.Text);
-                    if (lastOperator == 0)
-                    {
-                        textBoxExpression.Text = textBoxResult.Text + "+" + lastOperand + "=";
-                        textBoxResult.Text = (lastOperand + Convert.ToDouble(textBoxResult.Text)).ToString();
-                    }
-                    else if (lastOperator == 1)
-                    {
-                        textBoxExpression.Text = textBoxResult.Text + "-" + lastOperand + "=";
-                        textBoxResult.Text = (lastOperand - Convert.ToDouble(textBoxResult.Text)).ToString();
-                    }
-                    else if (lastOperator == 2)
-                    {
-                        textBoxExpression.Text = textBoxResult.Text + "*" + lastOperand + "=";
-                        textBoxResult.Text = (lastOperand * Convert.ToDouble(textBoxResult.Text)).ToString();
-                    }*/
+                    textBoxExpression.Text = textBoxResult.Text + " " + expression[1] + " " + expression[2] + " " + "=" + " ";//1+5*2=會錯
                 }
                 else
                 {
@@ -230,27 +184,8 @@ namespace _0312
                     {
                         inqueue.Enqueue(s);
                     }
-
-                    lastOperand = Convert.ToDouble(textBoxResult.Text);
                     textBoxResult.Text = eval(inqueue).ToString();
                     textBoxExpression.Text += " " + "=" + " ";
-
-                    /*textBoxExpression.Text += textBoxResult.Text + " " + "=" + " ";
-                     
-                      lastOperand = Convert.ToDouble(textBoxResult.Text);
-
-                    if (lastOperator == 0)
-                    {
-                        textBoxResult.Text = (operand + Convert.ToDouble(textBoxResult.Text)).ToString();
-                    }
-                    else if (lastOperator == 1)
-                    {
-                        textBoxResult.Text = (operand - Convert.ToDouble(textBoxResult.Text)).ToString();
-                    }
-                    else if (lastOperator == 2)
-                    {
-                        textBoxResult.Text = (operand * Convert.ToDouble(textBoxResult.Text)).ToString();
-                    }*/
                 }
             }
             equalPressed = true;
